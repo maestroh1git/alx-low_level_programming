ec0ce840d716a652c17ebcde2a569a6497fc3a85
Maestroh <amakudavid01@gmail.com>
2022-09-11 05:04:10 -0700

    fix: task4- betty-style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main- Entry point for the program
- *   *
- *    * Return: By default returns zero if no errors
- *     * Otherwise returns non-zero value.
- *      */
+ * main- Entry point for the program
+ * Return: By default returns zero if no errors
+ * Otherwise returns non-zero value.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
