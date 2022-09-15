a890d9ec4b32ddace245715da0d0eaeca17682da
Maestroh <amakudavid01@gmail.com>
2022-09-14 17:47:27 -0700

    add: task5 to task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include main.h
+
+/**
+ * print_to_98 -Print from n to 98
+ * @n: A input integer
+ * Description: function that prints all natural numbers
+ * from n to 98, followed by a new line.
+ * Return: print from n to 98
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf(n);
+	}
+	else
+		{
+			for (; n >= 98; n--)
+			{
+				printf("%d", n);
+				if (n == 98)
+					continue;
+				printf(", ");
+			}
+			printf(n);
+		}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an integer
+ * @n: test number
+ * Return: absolute value of @n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+		else
+	{
+		return (n);
+	}
+}
