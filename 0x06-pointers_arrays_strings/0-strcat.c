0983a32885a52c4b55361c3ace5ae7bf6c5176f1
Maestroh <amakudavid01@gmail.com>
2022-09-23 03:53:13 -0700

    fix: 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,17 +12,19 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
+	j = 0;
+
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		i++;
 		j++;
+		i++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
